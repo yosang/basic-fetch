cb1e4834ab534f9410dd383e35aba951b8d1c9d1
Yosmel Chiang <yosmelchiang@gmail.com>
2025-10-22 21:55:02 +0200

    add example using json

--- 1.fetch/2.response.js
+++ 1.fetch/2.response.js
@@ -2,6 +2,8 @@
 // HTTP RESPONSE - HEAD, BODY
 
 const imgStr = "https://picsum.photos/id/237/200/300";
+const textStr = "http://localhost:3000/";
+const jsonData = "http://localhost:3000/users"
 
 // We start by creating a POJO
 const obj = {
@@ -28,23 +30,63 @@ const response = new Response(file, {
 // Log the response
 // console.log(response);
 
-// Example
-function replaceImage() {
+// Examples
+
+function fetchData() {
+  // Makes a call to a website that returns an image, we store the image in memory as a blob
+  // We convert it to an url using the URL object and method createObjectURL
   fetch(imgStr)
     .then((response) => {
       if (!response.ok) throw new Error("Invalid response");
       return response.blob(); // binary large object - images, video, audio, fonts etc
 
-      response.text() // For text, html, xml files, css, javascript etc
-      response.json() // for json files
+      response.text(); // For text, html, xml files, css, javascript etc
+      response.json(); // for json files
     })
     .then((blob) => {
       const blobPath = URL.createObjectURL(blob);
-    //   console.log(blob)
-    //   console.log(blobPath);
+      //   console.log(blob)
+      //   console.log(blobPath);
 
-      const img = document.getElementById('pic');
-      img.src = blobPath
+      const img = document.getElementById("pic");
+      img.src = blobPath;
     })
     .catch((err) => console.log(err));
+
+  // Makes a call to our express server, which returns a response holding "Content-Type":"text/html"
+  fetch(textStr)
+    .then((response) => {
+      if (!response.ok) throw new Error("Invalid response");
+      
+      return response.text();
+    })
+    .then((data) => {
+      const hd = document.getElementById('header');
+      hd.textContent = data;
+    })
+    .catch((err) => console.log(err));
+
+    // Makes a call to our server, grabs the json and visualises it on the page
+    fetch(jsonData)
+      .then(response => response.json())
+      .then(data => {
+        const arrayOfUsers = [];
+
+        data.forEach(user => {
+          arrayOfUsers.push({
+            name: user.name,
+            company: user.company.name
+          })
+        })
+
+        const users = document.getElementById('users');
+        
+        arrayOfUsers.forEach(user => {
+          const listElement = document.createElement('li')
+          listElement.innerHTML = `<strong>${user.name}</strong>, works at: <strong>${user.company}</strong>`
+          users.append(listElement)
+        })
+        
+      })
+      .catch(err => console.log(err))
 }
